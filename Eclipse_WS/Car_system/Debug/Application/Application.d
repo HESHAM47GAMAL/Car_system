ef82d71118c8cbe7b684d314dd3398d9ad3b6264
Joe2Hell <muhammad.usf@outlook.com>
2024-03-09 11:45:38 +0200

    without  comments

---
+++ Eclipse_WS/Car_system/Debug/Application/Application.d
@@ -0,0 +1,46 @@
+Application/Application.o Application/Application.o:  \
+ ../Application/Application.c ../Application/../SERVICE/COMMON_MACROS.h \
+  ../Application/../SERVICE/STD_TYPES.h \
+  ../Application/../HAL/BUTTON/BUTTON_interface.h \
+  ../Application/../HAL/BUTTON/../../SERVICE/STD_TYPES.h \
+  ../Application/../HAL/BUTTON/BUTTON_config.h \
+  ../Application/../HAL/BUTTON/../../MCAL/GPIO/GPIO_interface.h \
+  ../Application/../HAL/BUTTON/../../MCAL/GPIO/../../SERVICE/STD_TYPES.h \
+  ../Application/../HAL/LED/LED_interface.h \
+  ../Application/../HAL/LED/../../SERVICE/STD_TYPES.h \
+  ../Application/../HAL/LED/LED_config.h \
+  ../Application/../HAL/LED/../../MCAL/GPIO/GPIO_interface.h \
+  ../Application/../MCAL/GPIO/GPIO_interface.h \
+  ../Application/../HAL/LCD/LCD_interface.h \
+  ../Application/../HAL/LCD/../../SERVICE/STD_TYPES.h \
+  ../Application/Application.h
+
+../Application/../SERVICE/COMMON_MACROS.h:
+
+../Application/../SERVICE/STD_TYPES.h:
+
+../Application/../HAL/BUTTON/BUTTON_interface.h:
+
+../Application/../HAL/BUTTON/../../SERVICE/STD_TYPES.h:
+
+../Application/../HAL/BUTTON/BUTTON_config.h:
+
+../Application/../HAL/BUTTON/../../MCAL/GPIO/GPIO_interface.h:
+
+../Application/../HAL/BUTTON/../../MCAL/GPIO/../../SERVICE/STD_TYPES.h:
+
+../Application/../HAL/LED/LED_interface.h:
+
+../Application/../HAL/LED/../../SERVICE/STD_TYPES.h:
+
+../Application/../HAL/LED/LED_config.h:
+
+../Application/../HAL/LED/../../MCAL/GPIO/GPIO_interface.h:
+
+../Application/../MCAL/GPIO/GPIO_interface.h:
+
+../Application/../HAL/LCD/LCD_interface.h:
+
+../Application/../HAL/LCD/../../SERVICE/STD_TYPES.h:
+
+../Application/Application.h:
